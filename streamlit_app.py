f093262ea91aaa8db28d74a8e6ec801f0d751044
raqrodrigu <124785277+raqrodrigu@users.noreply.github.com>
2023-02-08 13:46:09 +0100

    Update streamlit_app.py
    importamos la libreria pandas para leer datos de un csv
    rellenamos los toppics para los batidos con el csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,10 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#importamos pandas para leer nuestro CSV de s3 
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#pedimos que nos muestre los datos en la pagina
+streamlit.dataframe(my_fruit_list)
