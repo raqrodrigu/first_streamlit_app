facf4fe1171553ac14af66da3e5c6e65f97797cb
raqrodrigu <124785277+raqrodrigu@users.noreply.github.com>
2023-02-08 14:01:35 +0100

    Update streamlit_app.py
    Actualizamos el selector para que al elegir veas los nombres de las frutas

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#añadimos el indice con el nombre de la fruta
+my_fruit_list= my_fruit_list.set_index('Fruit')
 
 #Agregaremos un widget interactivo para el usuario llamado Selección múltiple que permitirá a los usuarios 
 #elegir las frutas que desean en sus batidos.
-#hasta este paso el selector se ponía en forma de números
+#hasta este paso el selector se ponía en forma de números (pero al añadir la fila 19 se soluciona)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #pedimos que nos muestre los datos en la pagina
