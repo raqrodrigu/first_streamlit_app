56d9f4c22eb33f69cb005b29b1d9f0a87e138e5b
raqrodrigu <124785277+raqrodrigu@users.noreply.github.com>
2023-02-15 18:35:10 +0100

    Update streamlit_app.py
    normalizamos el json y lo mostramos como tabla y eliminamos la linea de json

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
+
+#cogemos la version json yla normalizamos 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# lo mostramos como tabla
+streamlit.dataframe(fruityvice_normalized)
