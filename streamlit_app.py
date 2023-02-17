c83e8949e3ad69ed27820175430e75ab23c3ed44
raqrodrigu <124785277+raqrodrigu@users.noreply.github.com>
2023-02-17 17:27:18 +0100

    Update streamlit_app.py
    en lugar de sacar una unica linea sacamos la lista de frutas

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,12 +56,15 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT * from fruit_load_list")
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from fruit_load_list")
+#my_data_row = my_cur.fetchone()
+#cogemos varias filas no solo la de banana
+my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text("The fruit load list contains:")
 streamlit.header("The fruit load list contains:")
 #streamlit.text(my_data_row)
+#streamlit.dataframe(my_data_row)
+#mostramos todas las filas
 streamlit.dataframe(my_data_row)
-
